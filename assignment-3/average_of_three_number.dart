d65144f13b2a796cf01b2b4d47b4c9fc046053b2
ByteBird <113516708+byte-bird@users.noreply.github.com>
2022-09-18 07:46:22 +0100

    Update average_of_three_number.dart

--- assignment-3/average_of_three_number.dart
+++ assignment-3/average_of_three_number.dart
@@ -1,16 +1,21 @@
 import 'dart:io';
 
 void main() {
+  
+  // taking Input form user
   print("Enter the value of Number 1: ");
   double? a = double.parse(stdin.readLineSync()!);
   print("Enter the value of Number 2: ");
   double? b = double.parse(stdin.readLineSync()!);
   print("Enter the value of Number 3: ");
   double? c = double.parse(stdin.readLineSync()!);
-
+  
+  // calling the average funtion
   average(a, b, c);
 }
 
+
+// average funtion
 void average(double a, double b, double c) {
   double average = (a + b + c) / 3;
 
