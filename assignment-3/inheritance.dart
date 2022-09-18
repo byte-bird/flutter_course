19e636eb93b21955c5092dda92d29e0e737c2d4e
ByteBird <113516708+byte-bird@users.noreply.github.com>
2022-09-18 07:45:31 +0100

    Update inheritance.dart

--- assignment-3/inheritance.dart
+++ assignment-3/inheritance.dart
@@ -1,19 +1,26 @@
 void main() {
+  // object of class A and C
   C c = C();
   A a = A();
-
+  
+  // calling by Inheritance
   c.printName("Queue Solution BD");
   a.printName("ByteBird");
 }
 
+//Parant class
 class Y {
   printName(String s) {
     print(s);
   }
 }
 
+// X is child of Y
+
 class X extends Y {}
 
+
+// A and C are the child of X
 class A extends X {}
 
 class C extends X {}
